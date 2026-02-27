6b55d36298fb61040085b11424502946a78842c1
Leonardo Lima <leo2342005@gmail.com>
2026-02-27 19:21:20 -0300

    feat: adicionando primeira feature de adesão ao produto.

---
+++ src/main/java/com/itau/srv/gerenciamento/clientes/model/enums/TipoConta.java
@@ -0,0 +1,6 @@
+package com.itau.srv.gerenciamento.clientes.model.enums;
+
+public enum TipoConta {
+    MASTER,
+    FILHOTE
+}
---
+++ src/main/java/com/itau/srv/gerenciamento/clientes/repository/ClienteRepository.java
@@ -0,0 +1,15 @@
+package com.itau.srv.gerenciamento.clientes.repository;
+
+import com.itau.srv.gerenciamento.clientes.model.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
+
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+    @Query("SELECT c FROM Cliente c WHERE c.id = :id AND c.ativo = TRUE")
+    Optional<Cliente> findByIdAndAtivo(Long id);
+
+    @Query("SELECT c FROM Cliente c WHERE c.cpf = :cpf AND c.ativo = TRUE")
+    Optional<Cliente> findByCpfAndAtivo(String cpf);
+}
